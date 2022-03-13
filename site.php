af010519cda84bc714cd6b0a74646894ad2f876d
Nedas <neduska991@gmail.com>
2022-03-13 20:21:58 +0000

    Variables and data types

--- site.php
+++ site.php
@@ -13,6 +13,55 @@
         echo "<hr>";
         echo "<p>this is nice...</p>";
         echo "<hr>";
+
+        // Variables
+        $name = "Sam";
+        $age = 25;     
+        $come = "George";
+
+        $add1 = 5;
+        $add2 = 10;
+
+        echo "There once was a man named $name ";
+        echo "He was $age years old <br>";
+        echo "He really liked the name $name <br>";
+        echo "But didin't like being $age <br>";
+
+        echo "Will " .$come. " come? <br>";
+        echo $add1 + $add2;
+
+        // Global scope variable
+        $x = 5;
+        $y = 10;
+
+        function myTest() {
+            $GLOBALS["y"] = $GLOBALS["x"] + $GLOBALS["y"];
+        }
+
+        myTest();
+        echo $y;
+
+        $isTrue = true;
+        $isFalse = false;
+
+        // Delete variable
+        unset($isTrue);
+
+        // Arithmetic
+        $sum = 1+1; 
+        $difference = 2-1;
+        $product = 2*2;
+        $quotient = 2/1;
+
+        // Shorthand arithmetic
+        $number = 0;
+        $number += 1; // Increment $number by 1
+        echo $number++; // Prints 1 and adds 1 to it
+        $number /= $float; // Divide and assign the quotient to $number
+
+        $apples = "I have {$number} apples to eat today."
+
+
     
     ?>
 </body>
